c14b2da1e5b9ea0179993dcea4b4750efc6de7e9
AnhPNH020506 <phamnguyenhunganh475@gmail.com>
2026-06-17 00:38:05 +0700

    Update LoginController with ouput field id

--- src/java/dao/CustomerDAO.java
+++ src/java/dao/CustomerDAO.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dao;
 
-/**
- *
- * @author User
- */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import utils.DbUtils;
+
 public class CustomerDAO {
-    
+
+    public Integer getCustomerIdByAccountId(int accountId) {
+
+        String sql = "SELECT CustomerID FROM Customer WHERE AccountID = ?";
+
+        try (
+            Connection conn = DbUtils.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql)
+        ) {
+
+            ps.setInt(1, accountId);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                return rs.getInt("CustomerID");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 }
