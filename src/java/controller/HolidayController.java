e451442bbc697ce81d7252a28e1ff08da077f3e1
Hming1509 <nguyenminh150906@gmail.com>
2026-07-01 21:04:50 +0700

    Fix lỗi Holiday và PricingRule
    done

--- src/java/controller/HolidayController.java
+++ src/java/controller/HolidayController.java
@@ -38,10 +38,16 @@ public class HolidayController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("application/json; charset=UTF-8");
+        Map<String, Object> res = new HashMap<>();
         try {
-            response.getWriter().print(gson.toJson(dao.getHolidays()));
+            res.put("success", true);
+            res.put("data", dao.getHolidays());
+            response.getWriter().print(gson.toJson(res));
         } catch (Exception e) {
             response.setStatus(500);
+            res.put("success", false);
+            res.put("message", e.getMessage());
+            response.getWriter().print(gson.toJson(res));
         }
     }
 
